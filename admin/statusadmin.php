4a497d2802a025af054518fb9140599ccb2b96d6
rjonesbsink <mike20111991@gmail.com>
2026-07-15 22:01:04 -0400

    Bootstrap UI migration: admin/statusadmin.php (issue #40 phase 16) (#62)
    * Bootstrap UI migration: admin/statusadmin.php (issue #40 phase 16)
    
    Migrate the status (punch item) list page to the shared Bootstrap
    layout, using the simple $admin_leftnav_current sidebar mode, mirroring
    the officeadmin.php/groupadmin.php list-page migrations.
    
    Fixes the same latent escaping issue found throughout this migration:
    status names (punchitems/punchnext) and the color value were
    interpolated raw into hrefs/title/style attributes, allowing broken
    links, reflected XSS, and CSS-context injection for any value
    containing HTML metacharacters.
    
    * Fix php:S2003: use include_once instead of include in statusadmin.php

--- admin/statusadmin.php
+++ admin/statusadmin.php
@@ -3,72 +3,36 @@
 require_once '../lib/session.php';
 start_secure_session();
 
+include_once '../config.inc.php';
+include_once 'header_bootstrap.php';
+include_once 'topmain_bootstrap.php';
+echo "<title>$title - Status Summary</title>\n";
+
 $self = htmlentities($_SERVER['PHP_SELF']);
 $request = $_SERVER['REQUEST_METHOD'];
 $user_agent = $_SERVER['HTTP_USER_AGENT'];
 
-include '../config.inc.php';
-include 'header.php';
-include 'topmain.php';
-echo "<title>$title - Status Summary</title>\n";
-
 require_once '../lib/auth.php';
 require_valid_user();
 
-echo "<table width=100% height=89% border=0 cellpadding=0 cellspacing=1>\n";
-echo "  <tr valign=top>\n";
-echo "    <td class=left_main width=180 align=left scope=col>\n";
-echo "      <table class=hide width=100% border=0 cellpadding=1 cellspacing=0>\n";
-echo "        <tr><td class=left_rows height=11></td></tr>\n";
-echo "        <tr><td class=left_rows_headings height=18 valign=middle>Users</td></tr>\n";
-echo "        <tr><td class=left_rows height=18 align=left valign=middle><img src='../images/icons/user.png' alt='User Summary' />&nbsp;&nbsp;
-                <a class=admin_headings href='useradmin.php'>User Summary</a></td></tr>\n";
-echo "        <tr><td class=left_rows height=18 align=left valign=middle><img src='../images/icons/user_add.png' alt='Create New User' />&nbsp;&nbsp;
-                <a class=admin_headings href='usercreate.php'>Create New User</a></td></tr>\n";
-echo "        <tr><td class=left_rows height=18 align=left valign=middle><img src='../images/icons/magnifier.png' alt='User Search' />&nbsp;&nbsp;
-                <a class=admin_headings href='usersearch.php'>User Search</a></td></tr>\n";
-echo "        <tr><td class=left_rows height=33></td></tr>\n";
-echo "        <tr><td class=left_rows_headings height=18 valign=middle>Offices</td></tr>\n";
-echo "        <tr><td class=left_rows height=18 align=left valign=middle><img src='../images/icons/brick.png' alt='Office Summary' />&nbsp;&nbsp;
-                <a class=admin_headings href='officeadmin.php'>Office Summary</a></td></tr>\n";
-echo "        <tr><td class=left_rows height=18 align=left valign=middle><img src='../images/icons/brick_add.png' alt='Create New Office' />&nbsp;&nbsp;
-                <a class=admin_headings href='officecreate.php'>Create New Office</a></td></tr>\n";
-echo "        <tr><td class=left_rows height=33></td></tr>\n";
-echo "        <tr><td class=left_rows_headings height=18 valign=middle>Groups</td></tr>\n";
-echo "        <tr><td class=left_rows height=18 align=left valign=middle><img src='../images/icons/group.png' alt='Group Summary' />&nbsp;&nbsp;
-                <a class=admin_headings href='groupadmin.php'>Group Summary</a></td></tr>\n";
-echo "        <tr><td class=left_rows height=18 align=left valign=middle><img src='../images/icons/group_add.png' alt='Create New Group' />&nbsp;&nbsp;
-                <a class=admin_headings href='groupcreate.php'>Create New Group</a></td></tr>\n";
-echo "        <tr><td class=left_rows height=33></td></tr>\n";
-echo "        <tr><td class=left_rows_headings height=18 valign=middle colspan=2>In/Out Status</td></tr>\n";
-echo "        <tr><td class=current_left_rows height=18 align=left valign=middle><img src='../images/icons/application.png' alt='Status Summary' />
-                &nbsp;&nbsp;<a class=admin_headings href='statusadmin.php'>Status Summary</a></td></tr>\n";
-echo "        <tr><td class=left_rows height=18 align=left valign=middle><img src='../images/icons/application_add.png' alt='Create Status' />&nbsp;&nbsp;
-                <a class=admin_headings href='statuscreate.php'>Create Status</a></td></tr>\n";
-echo "        <tr><td class=left_rows height=33></td></tr>\n";
-echo "        <tr><td class=left_rows_headings height=18 valign=middle colspan=2>Miscellaneous</td></tr>\n";
-echo "        <tr><td class=left_rows height=18 align=left valign=middle><img src='../images/icons/clock.png' alt='Add/Edit/Delete Time' />
-                &nbsp;&nbsp;<a class=admin_headings href='timeadmin.php'>Add/Edit/Delete Time</a></td></tr>\n";
-echo "        <tr><td class=left_rows height=18 align=left valign=middle><img src='../images/icons/application_edit.png' alt='Edit System Settings' />
-                &nbsp;&nbsp;<a class=admin_headings href='sysedit.php'>Edit System Settings</a></td></tr>\n";
-echo "        <tr><td class=left_rows height=18 align=left valign=middle><img src='../images/icons/database_go.png'
-                alt='Upgrade Database' />&nbsp;&nbsp;&nbsp;<a class=admin_headings href='dbupgrade.php'>Upgrade Database</a></td></tr>\n";
-echo "      </table></td>\n";
-echo "    <td align=left class=right_main scope=col>\n";
-echo "      <table width=100% height=100% border=0 cellpadding=10 cellspacing=1>\n";
-echo "        <tr class=right_main_text>\n";
-echo "          <td valign=top>\n";
-echo "            <table width=60% align=center height=40 border=0 cellpadding=0 cellspacing=0>\n";
-echo "              <tr><th class=table_heading_no_color nowrap width=100% valign=top halign=left>Status Summary</th></tr>\n";
-echo "            </table>\n";
-echo "            <table class=table_border width=60% align=center border=0 cellpadding=0 cellspacing=0>\n";
-echo "              <tr><th class=table_heading nowrap width=7% align=left>&nbsp;</th>\n";
-echo "                <th class=table_heading nowrap width=23% align=left>Status Name</th>\n";
-echo "                <th class=table_heading nowrap width=23% align=left>On Punch</th>\n";
-echo "                <th class=table_heading nowrap width=37% align=left>Color</th>\n";
-echo "                <th class=table_heading nowrap width=4% align=center>In/Out</th>\n";
-echo "                <th class=table_heading nowrap width=3% align=center>Edit</th>\n";
-echo "                <th class=table_heading nowrap width=3% align=center>Delete</th></tr>\n";
+echo "<div class=\"container-fluid mt-3\">\n";
+echo "  <div class=\"row\">\n";
+$admin_leftnav_current = 'statusadmin.php';
+include_once 'leftnav_bootstrap.php';
+echo "    <div class=\"col-md-9\">\n";
+
+echo "      <h5>Status Summary</h5>\n";
+echo "      <div class=\"table-responsive\">\n";
+echo "      <table class=\"table table-sm table-bordered align-middle\">\n";
+echo "        <tr>\n";
+echo "          <th>&nbsp;</th>\n";
+echo "          <th>Status Name</th>\n";
+echo "          <th>On Punch</th>\n";
+echo "          <th>Color</th>\n";
+echo "          <th class=\"text-center\">In/Out</th>\n";
+echo "          <th class=\"text-center\">Edit</th>\n";
+echo "          <th class=\"text-center\">Delete</th>\n";
+echo "        </tr>\n";
 
 $row_count = 0;
 
@@ -89,25 +53,38 @@ while ($row = mysqli_fetch_array($result)) {
         $in_or_out_tmp = 'Out';
     }
 
-    echo "              <tr class=table_border bgcolor='$row_color'><td nowrap class=table_rows width=7%>&nbsp;$row_count</td>\n";
-    echo "                <td nowrap class=table_rows width=23%>&nbsp;<a class=footer_links title='Edit Status: $punchitem'
-                    href='statusedit.php?statusname=$punchitem'>$punchitem</a></td>\n";
-    echo "                <td nowrap class=table_rows width=23%>&nbsp;" . ($punchnext ? "&rarr;&nbsp;" : "") . "<a class=footer_links title='Edit Status: $punchnext'
-                    href='statusedit.php?statusname=$punchnext'>$punchnext</a></td>\n";
-    echo "                <td class=table_rows width=37% align=left style='color:$color';>&nbsp;$color</td>\n";
-    echo "                <td nowrap class=table_rows width=4% align=center>$in_or_out_tmp</td>\n";
+    $h_punchitem = htmlentities($punchitem);
+    $h_punchnext = htmlentities($punchnext);
+    $h_color = htmlentities($color);
+    $punchitem_qs = urlencode($punchitem);
+    $punchnext_qs = urlencode($punchnext);
+
+    echo "        <tr style=\"background-color:$row_color;\">\n";
+    echo "          <td>$row_count</td>\n";
+    echo "          <td><a title=\"Edit Status: $h_punchitem\" href=\"statusedit.php?statusname=$punchitem_qs\">$h_punchitem</a></td>\n";
+    echo "          <td>" . ($punchnext ? "&rarr;&nbsp;" : "")
+        . "<a title=\"Edit Status: $h_punchnext\" href=\"statusedit.php?statusname=$punchnext_qs\">$h_punchnext</a></td>\n";
+    echo "          <td style=\"color:$h_color;\">$h_color</td>\n";
+    echo "          <td class=\"text-center\">$in_or_out_tmp</td>\n";
 
     if ((strpos($user_agent, "MSIE 6")) || (strpos($user_agent, "MSIE 5")) || (strpos($user_agent, "MSIE 4")) || (strpos($user_agent, "MSIE 3"))) {
-        echo "                <td class=table_rows width=3% align=center><a style='color:#27408b;text-decoration:underline;' title=\"Edit Status: $punchitem\"
-                    href=\"statusedit.php?statusname=$punchitem\">Edit</a></td>\n";
-        echo "                <td class=table_rows width=3% align=center><a style='color:#27408b;text-decoration:underline;' title=\"Delete Status: $punchitem\"
-                    href=\"statusdelete.php?statusname=$punchitem\">Delete</a></td></tr>\n";
+        echo "          <td class=\"text-center\"><a title=\"Edit Status: $h_punchitem\"
+                    href=\"statusedit.php?statusname=$punchitem_qs\">Edit</a></td>\n";
+        echo "          <td class=\"text-center\"><a title=\"Delete Status: $h_punchitem\"
+                    href=\"statusdelete.php?statusname=$punchitem_qs\">Delete</a></td></tr>\n";
     } else {
-        echo "                <td class=table_rows width=3% align=center><a title=\"Edit Status: $punchitem\" href=\"statusedit.php?statusname=$punchitem\">
+        echo "          <td class=\"text-center\"><a title=\"Edit Status: $h_punchitem\"
+                    href=\"statusedit.php?statusname=$punchitem_qs\">
                     <img border=0 src='../images/icons/application_edit.png' /></a></td>\n";
-        echo "                <td class=table_rows width=3% align=center><a title=\"Delete Status: $punchitem\" href=\"statusdelete.php?statusname=$punchitem\">
+        echo "          <td class=\"text-center\"><a title=\"Delete Status: $h_punchitem\"
+                    href=\"statusdelete.php?statusname=$punchitem_qs\">
                     <img border=0 src='../images/icons/delete.png' /></a></td></tr>\n";
     }
 }
-echo "          </table></td></tr>\n";
-include '../footer.php';
+echo "      </table>\n";
+echo "      </div>\n";
+echo "    </div>\n";
+echo "  </div>\n";
+echo "</div>\n";
+include_once 'footer_bootstrap.php';
+exit;
